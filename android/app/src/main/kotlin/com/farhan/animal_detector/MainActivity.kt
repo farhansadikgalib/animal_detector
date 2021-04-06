8f3c8ae98c625d25e37c17216baaa90eeeb9da05
farhan <farhansadikgalib@gmail.com>
2021-04-06 21:11:00 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/farhan/animal_detector/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.farhan.animal_detector
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/SplashPage.dart
@@ -0,0 +1,31 @@
+import 'package:animal_detector/HomePage.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 4,
+      navigateAfterSeconds: HomePage(),
+
+      title: Text("Cat and Dog Detector",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold,color: Colors.orange),),
+      image: Image.asset("assets/icon.png"),backgroundColor: Colors.white,photoSize: 75,
+      loaderColor: Colors.orange,
+      loadingText: Text("Developed by Farhan",style: TextStyle(fontSize: 16,color: Colors.black),),
+      
+      
+
+
+
+
+
+
+    );
+  }
+}
